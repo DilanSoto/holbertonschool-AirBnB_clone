1129449450e08e47c9573d2eebf6159830b79e42
xov121 <x.o.v121@gmail.com>
2024-02-23 16:28:25 -0400

    Initial implementation of file_storage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,35 @@
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects."""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id."""
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)."""
+        obj_dict = {
+            obj_id: obj.to_dict()
+            for obj_id, obj in self.__objects.items()
+        }
+        with open(self.__file_path, 'w') as f:
+            json.dump(obj_dict, f)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects."""
+        try:
+            with open(self.__file_path, 'r') as f:
+                obj_dict = json.load(f)
+            for obj_id, obj_data in obj_dict.items():
+                self.__objects[obj_id] = BaseModel(**obj_data)
+        except FileNotFoundError:
+            pass
