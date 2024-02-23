b41e8e382f8756650d71fdd1aee5884670256c01
xov121 <x.o.v121@gmail.com>
2024-02-23 15:44:27 -0400

    Initial implementation of BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,42 @@
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """Defines all common attributes/methods for other classes"""
+
+    def __init__(self, *args, **kwargs):
+        """Initialization of the base model"""
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+        # If kwargs is not empty, set attributes accordingly
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                if key != "__class__":
+                    setattr(self, key, value)
+
+    def __str__(self):
+        """String representation of the BaseModel instance"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """
+        Updates the public instance attribute updated_at with the current
+        datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values of __dict__ of the
+        instance
+        """
+        result_dict = dict(self.__dict__)
+        result_dict["__class__"] = self.__class__.__name__
+        result_dict["created_at"] = self.created_at.isoformat()
+        result_dict["updated_at"] = self.updated_at.isoformat()
+        return result_dict
