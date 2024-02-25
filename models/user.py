719e8a75568024c9d01f466ebf7bf8dd55c2dea0
xov121 <x.o.v121@gmail.com>
2024-02-24 23:03:04 -0400

    Implemented User class

---
+++ models/user.py
@@ -0,0 +1,17 @@
+from .base_model import BaseModel
+
+
+class User(BaseModel):
+    """Represents a user with email, password, first name, and last name"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.email = kwargs.get('email', "")
+        self.password = kwargs.get('password', "")
+        self.first_name = kwargs.get('first_name', "")
+        self.last_name = kwargs.get('last_name', "")
