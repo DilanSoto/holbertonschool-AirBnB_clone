f99f7eceba771837a9df9f6fdce4a221c9105db4
Javier Gabriel Marrero Narvaez <135934256+xov121@users.noreply.github.com>
2024-02-28 03:02:06 -0400

    Reverted to previous base_model.py because of less checkers passed with new code.

--- models/base_model.py
+++ models/base_model.py
@@ -1,94 +1,49 @@
-#!/usr/bin/python3
-
-# & This module defines a base class for all models in our hbnb clone
-
-# & Import necessary modules
-
-import uuid
-
+from uuid import uuid4
 from datetime import datetime
-
-import models
-
-
-
 
 
 class BaseModel:
-
-    """A base class for all hbnb models
-
-    that defines all common attributes/methods
-
-    for other classes"""
-
-    # & Initialize a new BaseModel
-
-
+    """Defines all common attributes/methods for other classes"""
 
     def __init__(self, *args, **kwargs):
-        """
-        If kwargs are provided, set them as attributes.
-        Otherwise, set default attributes.
-        """
+        """Initialization of the base model"""
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+        valid_attrs = ["id", "created_at", "updated_at", "name", "my_number"]
+
         if kwargs:
             for key, value in kwargs.items():
-                # & Ignore __class__ attribute
-                if key == "__class__":
-                    pass
-                # & Convert string datetime to datetime object
-                elif key == "created_at" or key == "updated_at":
-                    setattr(self, key,
-                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-                # & Set other attributes
-                else:
+                if key in valid_attrs:
+                    if key in ["created_at", "updated_at"]:
+                        try:
+                            value = datetime.fromisoformat(value)
+                        except ValueError:
+                            continue
                     setattr(self, key, value)
-        else:
-            # & Set default attributes
-            self.id = str(uuid.uuid4())  # & Create a unique UUID
-            self.created_at = datetime.now()  # & Set the creation time to now
-            self.updated_at = datetime.now()  # & Set the update time to now
-            models.storage.new(self)  # & Add the new instance to storage
-
-
-    # & Return a string representation of the BaseModel instance
 
     def __str__(self):
-
-        return F"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-
-
-
-    # & Update updated_at with the current datetime and save to storage
+        """String representation of the BaseModel instance"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-
+        """
+        Updates the public instance attribute updated_at with the current
+        datetime
+        """
         self.updated_at = datetime.now()
-
-        models.storage.new(self)
-
-        models.storage.save()
-
-
-
-    # & Return a dictionary representation of the BaseModel instance
+        # Import here to avoid circular import issues
+        from models import storage
+        storage.new(self)
+        storage.save()
 
     def to_dict(self):
-
         """
-
-        Convert datetime objects to ISO format strings.
-
+        Returns a dictionary containing all keys/values of __dict__ of the
+        instance
         """
-
-        new_dict = self.__dict__.copy()
-
-        new_dict["__class__"] = self.__class__.__name__
-
-        # & Convert datetime objects to ISO format strings
-
-        new_dict["created_at"] = self.created_at.isoformat()
-
-        new_dict["updated_at"] = self.updated_at.isoformat()
-
-        return new_dict
+        result_dict = dict(self.__dict__)
+        result_dict["__class__"] = self.__class__.__name__
+        result_dict["created_at"] = self.created_at.isoformat()
+        result_dict["updated_at"] = self.updated_at.isoformat()
+        return result_dict
