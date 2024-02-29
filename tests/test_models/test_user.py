0aaec47bbab7f9eb177122d7837edfb25da29fd0
xov121 <x.o.v121@gmail.com>
2024-02-29 02:54:35 -0400

    Testing enhanced User unitttests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -6,22 +6,41 @@ from models.user import User
 
 
 class TestUser(unittest.TestCase):
-    """testing the correct functioning of the class"""
-    def test_str_attr(self):
-        """test attributes"""
-        user = User()
-        self.assertEqual(user.email, "")
-        self.assertEqual(user.password, "")
-        self.assertEqual(user.first_name, "")
-        self.assertEqual(user.last_name, "")
+    """Testing the correct functioning of the User class."""
 
-    def test_str(self):
-        """attribute type test"""
-        user = User()
-        self.assertEqual(type(user.email), str)
-        self.assertEqual(type(user.password), str)
-        self.assertEqual(type(user.first_name), str)
-        self.assertEqual(type(user.last_name), str)
+    def setUp(self):
+        """Set up for the tests."""
+        self.user = User()
+
+    def tearDown(self):
+        """Clean up after the tests."""
+        del self.user
+
+    def test_str_attr_initialization(self):
+        """Test attributes initialization."""
+        self.assertEqual(self.user.email, "")
+        self.assertEqual(self.user.password, "")
+        self.assertEqual(self.user.first_name, "")
+        self.assertEqual(self.user.last_name, "")
+
+    def test_str_attr_types(self):
+        """Attribute type test."""
+        self.assertIsInstance(self.user.email, str)
+        self.assertIsInstance(self.user.password, str)
+        self.assertIsInstance(self.user.first_name, str)
+        self.assertIsInstance(self.user.last_name, str)
+
+    def test_setting_attributes(self):
+        """Test setting attribute values."""
+        self.user.email = "user@example.com"
+        self.user.password = "securepassword"
+        self.user.first_name = "John"
+        self.user.last_name = "Doe"
+
+        self.assertEqual(self.user.email, "user@example.com")
+        self.assertEqual(self.user.password, "securepassword")
+        self.assertEqual(self.user.first_name, "John")
+        self.assertEqual(self.user.last_name, "Doe")
 
 
 if __name__ == "__main__":
